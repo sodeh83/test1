6c2c670cb4f623f6b560b95f8e7f79bd2b41a08a
sodeh <sodeh.tarahomi@yahoo.com>
2019-11-05 20:00:49 -0500

    test1

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your num:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            while (num != 1)
+            {
+                result = result * (num * (num - 1));
+                num--;
+            }
+
+            Console.WriteLine("The result is: {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
+
